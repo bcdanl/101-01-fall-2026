e528cbe00dacf3e6fdce6e2bca07a6e7c0233a4c
bcdanl <bchoe@geneseo.edu>
2026-09-02 17:48:52 -0400

    0902

---
+++ danl-101-01-class-2026-0902.R
@@ -0,0 +1,18 @@
+a <- 1
+b <- 2
+
+# Quotation marks for the package name is necessary
+#   when using install.packages()
+# install.packages("tidyverse")
+
+library(tidyverse)
+mpg
+ggplot2::mpg
+
+df <- read_csv("https://bcdanl.github.io/data/spotify_all.csv")
+
+
+
+
+
+
